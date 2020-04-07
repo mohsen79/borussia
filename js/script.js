639038db61d784fa786047f2a2dfdd020d54e09b
mohsen79 <mohsenjalali.developer@gmail.com>
2020-04-07 23:52:29 +0430

    modify1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
-	
+	$('.menu i').click(function(){
+		if($('.menu').attr('data') == 'close'){
+		$('.search').slideDown();
+		$(this).removeClass('fa fa-search');
+		$(this).addClass('fas fa-times');
+		$('.menu').attr('data','open');
+	}else{
+		$('.search').slideUp();
+		$(this).removeClass('fas fa-times');
+		$(this).addClass('fa fa-search');
+		$('.menu').attr('data','close');
+	}
+	});
 });
