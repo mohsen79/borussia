10c991a823e54a2f63047ec687d18bb8fba323a3
mohsen79 <mohsenjalali.developer@gmail.com>
2020-04-08 17:00:07 +0430

    light on menu

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$('.menu i').click(function(){
+	$('#slide').click(function(){
 		if($('.menu').attr('data') == 'close'){
 		$('.search').slideDown();
 		$(this).removeClass('fa fa-search');
@@ -11,5 +11,18 @@ $(document).ready(function(){
 		$(this).addClass('fa fa-search');
 		$('.menu').attr('data','close');
 	}
+	});
+	$('#light').click(function(){
+		if($(this).attr('data') == 'off'){
+		$('body').addClass('body');
+		$(this).removeClass('fas fa-lightbulb');
+		$(this).addClass('far fa-lightbulb');
+		$(this).attr('data','on');
+		}else{
+			$('body').removeClass('body');
+			$(this).removeClass('far fa-lightbulb');
+			$(this).addClass('fas fa-lightbulb');
+			$(this).attr('data','off');
+		}
 	});
 });
